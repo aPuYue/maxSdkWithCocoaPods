7fe9400785562d3ee6751f56093a6fc4cca8a18e
aPuYue <pu.yue@access-company.com>
2022-07-19 21:24:00 +0900

    1

---
+++ maxSdkWithCocoaPods/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  maxSdkWithCocoaPods
+//
+//  Created by Pu Yue - PU YUE on 2022/07/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
