89732f3d56b5b8f0229d5ae19b76c9f969d4cb57
aPuYue <pu.yue@access-company.com>
2022-07-19 21:34:37 +0900

    Initialize the SDK

--- maxSdkWithCocoaPods/AppDelegate.swift
+++ maxSdkWithCocoaPods/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AppLovinSDK
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        ALSdk.shared()!.mediationProvider = "max"
+        ALSdk.shared()!.initializeSdk { (configuration: ALSdkConfiguration) in
+            if #available(iOS 14.5, *) {}
+        }
+        
         return true
     }
 
